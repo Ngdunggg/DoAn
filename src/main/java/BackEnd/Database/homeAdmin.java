b9c0a90e8307592c4c13d50d0a0479e05ff1ce29
Vu_Cong_Ngon <vucongngon219@gmail.com>
2024-03-26 22:42:05 +0700

    add: home admin

---
+++ src/main/java/BackEnd/Database/homeAdmin.java
@@ -0,0 +1,10 @@
+package BackEnd.Database;
+
+import java.sql.Statement;
+
+public class homeAdmin {
+    private static Statement st = database.getSt();
+    public addEmployee(String mnv, ) {
+
+    }
+}
