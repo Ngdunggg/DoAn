84d437a7d7015fcfdf8cf85f0b184ae2d65aec6a
Vu_Cong_Ngon <vucongngon219@gmail.com>
2024-03-26 23:12:22 +0700

    checkout to resole conflict

--- src/main/java/BackEnd/Database/homeAdmin.java
+++ src/main/java/BackEnd/Database/homeAdmin.java
@@ -1,10 +1,13 @@
 package BackEnd.Database;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class homeAdmin {
     private static Statement st = database.getSt();
-    public addEmployee(String mnv, ) {
+    public void addEmployee(String mnv, String HoVaTen, String yob, String address, String role, String username, String password) {
+    
+
 
     }
 }
