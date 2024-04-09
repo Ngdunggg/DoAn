c4b5433b89e929c8e9c28c38eb6e93609ed8daae
Vu_Cong_Ngon <vucongngon219@gmail.com>
2024-04-09 07:20:48 +0700

    add: doten

---
+++ src/main/java/BackEnd/Database/dotenv.java
@@ -0,0 +1,7 @@
+package BackEnd.Database;
+
+public class dotenv {
+    public static  String Postgresql = "jdbc:postgresql://localhost:5432/db-do-an";
+    public static  String password = "hanhtinhsongsong";
+    public static  String name = "postgres";
+}
